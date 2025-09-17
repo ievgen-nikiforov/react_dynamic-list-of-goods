48ed7b96a2276f2d753c17621f97f667c77f9965
inikiforov <ievgen.nikiforov@tedata.de>
2025-09-17 15:42:08 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
+export const App: React.FC = () => {
+  const [goods, setGoods] = React.useState<Good[]>([]);
+function showGoods(getGoods: () => Promise<Good[]>) {
+  getGoods().then((newGoods) => setGoods(newGoods));
+  console.log(
+  );
+}
+  return (
   <div className="App">
     <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button">
+    <button type="button" data-cy="all-button" onClick={()=>showGoods(getAll)}>
       Load all goods
     </button>
 
-    <button type="button" data-cy="first-five-button">
+    <button type="button" data-cy="first-five-button" onClick={()=>showGoods(get5First)}>
       Load 5 first goods
     </button>
 
-    <button type="button" data-cy="red-button">
+    <button type="button" data-cy="red-button" onClick={()=>showGoods(getRedGoods)}>
       Load red goods
     </button>
 
-    <GoodsList goods={[]} />
+    <GoodsList goods={goods} />
   </div>
-);
+  )
+}
